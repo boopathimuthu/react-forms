421449aa44af3dc267ba5d10de58953990235c4c
boopathimuthu <69614811+boopathimuthu@users.noreply.github.com>
2021-12-07 20:36:41 +0530

    react form1

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Forms from './Forms'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+     <Forms/>
+    </div>
+  );
+}
+
+export default App;
